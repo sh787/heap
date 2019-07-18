bb6892f61b240fd194287cca321d1e2e27309405
Sophia <Sophia@172.20.12.204>
2019-07-18 17:55:57 -0400

    Pair programmed with Julia Chang: created Heap class

---
+++ A4/src/a4/Heap.java
@@ -0,0 +1,44 @@
+package a4;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class Heap<E, P> implements PriorityQueue<E,P>{
+
+	@Override
+	public Comparator<? super P> comparator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E poll() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(E e, P p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changePriority(E e, P p) throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
