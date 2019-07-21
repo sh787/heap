2c3ca6c1a5845474c2481fbd08f689cbc2a82897
Sophia <Sophia@172.20.12.204>
2019-07-21 15:14:45 -0400

    Pair programmed with Julia Chang: added test methods

--- A4/src/a4/HeapTest.java
+++ A4/src/a4/HeapTest.java
@@ -6,8 +6,33 @@ import org.junit.jupiter.api.Test;
 class HeapTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor() {
+		throw new NotImplementedError();
+	}
+	
+	@Test
+	void testSize() {
+		throw new NotImplementedError();
+	}
+	
+	@Test
+	void testPoll() {
+		throw new NotImplementedError();
+	}
+	
+	@Test
+	void testPeek() {
+		throw new NotImplementedError();
+	}
+	
+	@Test
+	void testAddl() {
+		throw new NotImplementedError();
+	}
+	
+	@Test
+	void testChangePriority() {
+		throw new NotImplementedError();
 	}
 
 }
